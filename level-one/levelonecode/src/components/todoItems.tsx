b97e358ba7d50a55bfd9cc3c364338f117f812ec
Mesha Vegas <84249301+Meshavegas@users.noreply.github.com>
2024-10-03 22:33:18 +0100

    add level One

---
+++ level-one/levelonecode/src/components/todoItems.tsx
@@ -0,0 +1,57 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+type Props = {
+  title: string;
+  subTitle: string;
+};
+const TodoItems = ({title, subTitle}: Props) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.titleStyle}>{title}</Text>
+        <Text style={styles.subTitleStyle}>{subTitle}</Text>
+      </View>
+      <View style={styles.actionContainer}>
+        <Text style={styles.iconStyle}>Ed</Text>
+        <Text style={styles.iconStyle}>Del</Text>
+        <Text style={styles.iconStyle}>Cple</Text>
+      </View>
+    </View>
+  );
+};
+export default TodoItems;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    backgroundColor: '#DCD8CB',
+    borderRadius: 15,
+    marginTop: 12,
+  },
+  titleStyle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#163C45',
+  },
+  subTitleStyle: {
+    fontSize: 16,
+    color: '#000',
+  },
+  iconStyle: {
+    fontSize: 20,
+    color: '#163C45',
+  },
+  textContainer: {
+    flex: 2,
+  },
+  actionContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
